9e66464894639a17fe0783957707c39e48f7ce61
Jan Petr <honzik.petr@gmail.com>
2018-07-18 17:37:03 +0200

    Improve query rules quota message

--- Model/Observer/Merchandising.php
+++ Model/Observer/Merchandising.php
@@ -6,6 +6,8 @@ use Algolia\AlgoliaSearch\Helper\MerchandisingHelper;
 use Magento\Framework\App\RequestInterface;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Phrase;
 use Magento\Store\Model\StoreManagerInterface;
 
 class Merchandising implements ObserverInterface
@@ -26,7 +28,7 @@ class Merchandising implements ObserverInterface
 
     public function execute(Observer $observer)
     {
-        $categoryId = $this->request->getParam('entity_id'); // TODO: Test on EE
+        $categoryId = $this->request->getParam('entity_id');
         $positions = $this->request->getParam('algolia_merchandising_positions');
 
         // The merchandising tab was not opened
@@ -36,17 +38,33 @@ class Merchandising implements ObserverInterface
 
         $positions = json_decode($positions, true);
 
-        foreach ($this->storeManager->getStores() as $store) {
-            if (!$store->getIsActive()) {
-                continue;
+        try {
+            foreach ($this->storeManager->getStores() as $store) {
+                if (!$store->getIsActive()) {
+                    continue;
+                }
+
+                if (!$positions) {
+                    $this->merchandisingHelper->deleteQueryRule($store->getId(), $categoryId);
+
+                    return;
+                }
+
+                $this->merchandisingHelper->saveQueryRule($store->getId(), $categoryId, $positions);
+            }
+        } catch(\AlgoliaSearch\AlgoliaException $e) {
+            $message = $e->getMessage();
+
+            if ($message === 'Rules quota exceeded. Please contact us if you need an extended quota.') {
+                $message = '
+                    The category cannot be merchandised with Algolia 
+                    as you hit your <a href="https://www.algolia.com/pricing/" target="_blank">query rules quota</a>. 
+                    If you need an extended quota, 
+                    please contact us on <a href="mailto:support@algolia.com">support@algolia.com</a>.';
             }
 
-            if (!$positions) {
-                $this->merchandisingHelper->deleteQueryRule($store->getId(), $categoryId);
-                return;
-            }
-
-            $this->merchandisingHelper->saveQueryRule($store->getId(), $categoryId, $positions);
+            $phrase = new Phrase($message);
+            throw new LocalizedException($phrase);
         }
     }
 }

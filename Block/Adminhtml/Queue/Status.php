a91a7825ab6fbc99fc3a45dcd394f10fb9dfeaa8
Damien Couchez <damien.couchez@gmail.com>
2018-08-14 10:10:15 +0200

    Refactor notices display

--- Block/Adminhtml/Queue/Status.php
+++ Block/Adminhtml/Queue/Status.php
@@ -95,12 +95,42 @@ class Status extends Template
         return $this->getUrl('*/*/reset');
     }
 
+    public function getNotices()
+    {
+        $notices = [];
+
+        if ($this->isQueueStuck()) {
+            $notices[] = '<a href="' . $this->getResetQueueUrl() .'"> ' . __('Reset queue') . '</a>';
+        }
+
+        if ($this->isQueueNotProcessed()) {
+            $notices[] =  __(
+                'We noticed that the queue has not been processed for one hour. Can you please check its status and if you correctly set up the cron?'
+            );
+            $notices[] =  __(
+                'To help you, please read our <a href="%1" target="_blank">documentation</a>.',
+                'https://community.algolia.com/magento/doc/m2/indexing-queue/'
+            );
+        }
+
+        if ($this->isQueueFast()) {
+            $notices[] = __(
+                'We noticed that the average processing time of the queue is under one minute during the two last past days.'
+            );
+            $notices[] = __(
+                'Please consider to increase the number of jobs to process to speed the queue if you want.'
+            );
+        }
+
+        return $notices;
+    }
+
     /**
      * If the queue status is not "ready" and it is running for more than 5 minutes, we consider that the queue is stuck
      *
      * @return boolean
      */
-    public function isQueueStuck()
+    private function isQueueStuck()
     {
         if ($this->queueRunnerIndexer->getStatus() == \Magento\Framework\Indexer\StateInterface::STATUS_VALID) {
             return false;
@@ -118,7 +148,7 @@ class Status extends Template
      *
      * @return boolean
      */
-    public function isQueueNotProcessed()
+    private function isQueueNotProcessed()
     {
         return $this->getTimeSinceLastIndexerUpdate() > self::QUEUE_NOT_PROCESSED_LIMIT;
     }
@@ -128,7 +158,7 @@ class Status extends Template
      *
      * @return boolean
      */
-    public function isQueueFast()
+    private function isQueueFast()
     {
         $averageProcessingTime = $this->queue->getAverageProcessingTime();
         return !is_null($averageProcessingTime) && $averageProcessingTime < self::QUEUE_FAST_LIMIT;

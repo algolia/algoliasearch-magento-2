586971f67040d3949958752587650a9f9af7fce4
Jan Petr <honzik.petr@gmail.com>
2018-11-15 11:34:09 +0100

    Send addition info to HS

--- Helper/SupportHelper.php
+++ Helper/SupportHelper.php
@@ -4,6 +4,7 @@ namespace Algolia\AlgoliaSearch\Helper;
 
 use Magento\Framework\App\ResourceConnection;
 use Magento\Framework\DB\Adapter\AdapterInterface;
+use Magento\Store\Model\StoreManagerInterface;
 
 class SupportHelper
 {
@@ -15,6 +16,9 @@ class SupportHelper
     /** @var AdapterInterface */
     private $dbConnection;
 
+    /** @var StoreManagerInterface */
+    private $storeManager;
+
     /** @var string */
     private $queueTable;
 
@@ -25,20 +29,25 @@ class SupportHelper
     private $configTable;
 
     /** @var string */
+    private $catalogTable;
+
+    /** @var string */
     private $modulesTable;
 
     /**
      * @param ConfigHelper $configHelper
      * @param ResourceConnection $resourceConnection
      */
-    public function __construct(ConfigHelper $configHelper, ResourceConnection $resourceConnection)
+    public function __construct(ConfigHelper $configHelper, ResourceConnection $resourceConnection, StoreManagerInterface $storeManager)
     {
         $this->configHelper = $configHelper;
         $this->dbConnection = $resourceConnection->getConnection('core_read');
+        $this->storeManager = $storeManager;
 
         $this->queueTable = $resourceConnection->getTableName('algoliasearch_queue');
         $this->configTable = $resourceConnection->getTableName('core_config_data');
         $this->queueArchiveTable = $resourceConnection->getTableName('algoliasearch_queue_archive');
+        $this->catalogTable = $resourceConnection->getTableName('catalog_product_entity');
         $this->modulesTable = $resourceConnection->getTableName('setup_module');
     }
 
@@ -129,17 +138,58 @@ class SupportHelper
      */
     private function getNoteText()
     {
-        $noteText = '';
+        $noteText = [];
 
-        $noteText .= '<b>Extension version:</b> ' . $this->getExtensionVersion() . '<br>';
-        $noteText .= '<b>Magento version:</b> ' . $this->configHelper->getMagentoVersion() . '<br>';
-        $noteText .= '<b>Magento edition:</b> ' . $this->configHelper->getMagentoEdition() . '<br><br>';
+        $noteText[] = $this->getGeneralMagentoInfo();
+        $noteText[] = $this->getQueueInfo();
+        $noteText[] = $this->getQueueArchiveInfo();
+        $noteText[] = $this->getAlgoliaConfiguration();
+
+        // TODO: condition it
+        $noteText[] = $this->getCatalogInfo();
+        $noteText[] = $this->get3rdPartyModules();
+
+        $noteText = implode('<br><br>', $noteText);
+
+        return $noteText;
+    }
+
+    /** @return string */
+    private function getGeneralMagentoInfo()
+    {
+        $magentoInfo = [];
+
+        $magentoInfo[] = '<b>Extension version:</b> ' . $this->getExtensionVersion();
+        $magentoInfo[] = '<b>Magento version:</b> ' . $this->configHelper->getMagentoVersion();
+        $magentoInfo[] = '<b>Magento edition:</b> ' . $this->configHelper->getMagentoEdition();
+
+        return implode('<br>', $magentoInfo);
+    }
+
+    /**
+     * @return string
+     * @throws \Zend_Db_Statement_Exception
+     */
+    private function getQueueInfo()
+    {
+        $queueInfoText = [];
 
         $query = 'SELECT COUNT(*) as `count`, MIN(created) as `oldest` FROM ' . $this->queueTable;
         $queueInfo = $this->dbConnection->query($query)->fetch();
 
-        $noteText .= '<b>Number of jobs in indexing queue:</b> ' . $queueInfo['count'] . '<br>';
-        $noteText .= '<b>Oldest job in indexing queue was created at:</b> ' . $queueInfo['oldest'] . '<br><br>';
+        $queueInfoText[] = '<b>Number of jobs in indexing queue:</b> ' . $queueInfo['count'];
+        $queueInfoText[] = '<b>Oldest job in indexing queue was created at:</b> ' . $queueInfo['oldest'];
+
+        return implode('<br>', $queueInfoText);
+    }
+
+    /**
+     * @return string
+     * @throws \Zend_Db_Statement_Exception
+     */
+    private function getQueueArchiveInfo()
+    {
+        $queueArchiveInfo = [];
 
         $query = 'SELECT * 
           FROM '.$this->queueArchiveTable.' 
@@ -149,18 +199,80 @@ class SupportHelper
         $archiveRows = $this->dbConnection->query($query)->fetchAll();
         if ($archiveRows)
         {
-            $noteText .= "<b>Queue archive rows (20 newest rows):</b><br>";
+            $queueArchiveInfo[] = '<b>Queue archive rows (20 newest rows):</b>';
 
             $firstRow = reset($archiveRows);
             $headers = array_keys($firstRow);
-            $noteText .= implode(' || ', $headers) . '<br>';
+            $noteText[] = implode(' || ', $headers);
 
             $archiveRows = array_map(function($row) {
                 return implode(' || ', $row);
             }, $archiveRows);
 
-            $noteText .= implode('<br>', $archiveRows);
-            $noteText .= '<br><br>';
+            $queueArchiveInfo = array_merge($queueArchiveInfo, $archiveRows);
+        }
+
+        return implode('<br>', $queueArchiveInfo);
+    }
+
+    /**
+     * @return string
+     * @throws \Zend_Db_Statement_Exception
+     */
+    private function getAlgoliaConfiguration()
+    {
+        $configurationText = [];
+        $defaultConfigValues = [];
+
+        $configRows = $this->dbConnection->query($this->getConfigurationQuery())
+                                         ->fetchAll(\PDO::FETCH_KEY_PAIR);
+
+        $configurationText[] = '<b>Algolia configuration (default):</b>';
+        foreach ($configRows as $path => $value) {
+            $value = $this->getConfigurationValue($value);
+
+            $configurationText[] = $path . ' => ' . $value;
+            $defaultConfigValues[$path] = $value;
+        }
+
+        $storeIds = array_keys($this->storeManager->getStores());
+
+        foreach ($storeIds as $storeId) {
+            $configRows = $this->dbConnection->query($this->getConfigurationQuery($storeId))
+                                             ->fetchAll(\PDO::FETCH_KEY_PAIR);
+
+            $differentStoreConfigValues = [];
+            foreach ($configRows as $path => $value) {
+                $value = $this->getConfigurationValue($value);
+
+                if ($defaultConfigValues[$path] !== $value) {
+                    $differentStoreConfigValues[] = $path . ' => ' . $value;
+                }
+            }
+
+            if ($differentStoreConfigValues !== []) {
+                $configurationText[] = '<br>'; // Separator from previous config section
+                $configurationText[] = '<b>Algolia configuration (STORE ID ' . $storeId . '):</b>';
+                $configurationText = array_merge($configurationText, $differentStoreConfigValues);
+            }
+        }
+
+        return implode('<br>', $configurationText);
+    }
+
+    /**
+     * @param integer|null $storeId
+     *
+     * @return string
+     */
+    private function getConfigurationQuery($storeId = null)
+    {
+        $scope = 'default';
+        $scopeId = 0;
+
+        if ($storeId !== null) {
+            $scope = 'stores';
+            $scopeId = $storeId;
         }
 
         $query = 'SELECT 
@@ -169,21 +281,57 @@ class SupportHelper
           FROM 
             '.$this->configTable.' 
           WHERE 
-            path LIKE "algoliasearch_%"
+            scope = "'.$scope.'"
+            AND scope_id = '.$scopeId.'
+            AND path LIKE "algoliasearch_%"
             AND path != "algoliasearch_credentials/credentials/api_key"';
 
-        $configRows = $this->dbConnection->query($query)
-                                         ->fetchAll(\PDO::FETCH_KEY_PAIR);
+        return $query;
+    }
 
-        $noteText .= "<b>Algolia configuration (default):</b><br>";
-        foreach ($configRows as $path => $value) {
-            $value = json_decode($value, true) ?: $value;
-            $value = var_export($value, true);
+    /**
+     * @param string $value
+     *
+     * @return string
+     */
+    private function getConfigurationValue($value)
+    {
+        $value = json_decode($value, true) ?: $value;
+        $value = var_export($value, true);
 
-            $noteText .= $path . ' => ' . $value . '<br>';
+        return $value;
+    }
+
+    /**
+     * @return string
+     * @throws \Zend_Db_Statement_Exception
+     */
+    private function getCatalogInfo()
+    {
+        $catalogInfoText = ['<b>Catalog size (by product type):</b>'];
+
+        $query = 'SELECT type_id, COUNT(*) as `count` FROM ' . $this->catalogTable . ' GROUP BY type_id';
+        $catalogInfo = $this->dbConnection->query($query)->fetchAll(\PDO::FETCH_KEY_PAIR);
+
+        $total = 0;
+        foreach ($catalogInfo as $type => $count) {
+            $total += $count;
+
+            $catalogInfoText[] = $type . ': ' . number_format($count, 0, ',', ' ');
         }
 
-        $noteText .= '<br><br>';
+        $catalogInfoText[] = 'Total number: ' . number_format($total, 0, ',', ' ');
+
+        return implode('<br>', $catalogInfoText);
+    }
+
+    /**
+     * @return string
+     * @throws \Zend_Db_Statement_Exception
+     */
+    private function get3rdPartyModules()
+    {
+        $modulesText = [];
 
         $query = 'SELECT * 
           FROM '.$this->modulesTable.' 
@@ -192,20 +340,19 @@ class SupportHelper
 
         $modules = $this->dbConnection->query($query)->fetchAll();
         if ($modules) {
-            $noteText .= "<b>3rd party modules:</b><br>";
+            $modulesText[] = '<b>3rd party modules:</b>';
             $firstRow = reset($modules);
             $headers = array_keys($firstRow);
-            $noteText .= implode(' || ', $headers) . '<br>';
+            $modulesText[] = implode(' || ', $headers);
 
             $modules = array_map(function($row) {
                 return implode(' || ', $row);
             }, $modules);
 
-            $noteText .= implode('<br>', $modules);
-            $noteText .= '<br><br>';
+            $modulesText = array_merge($modulesText, $modules);
         }
 
-        return $noteText;
+        return implode('<br>', $modulesText);
     }
 
     /**

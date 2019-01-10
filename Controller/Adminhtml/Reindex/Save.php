6a6bfb0ec9ffe7191ef95dbfa2f836878afcc72b
Betty Suravech <bsuravech@gmail.com>
2019-01-10 13:43:56 -0800

    move around exceptions for store/website specific values to try catch in storeId loop so that it does not exception out completely on the first store

--- Controller/Adminhtml/Reindex/Save.php
+++ Controller/Adminhtml/Reindex/Save.php
@@ -10,7 +10,7 @@ use Algolia\AlgoliaSearch\Exception\UnknownSkuException;
 use Algolia\AlgoliaSearch\Helper\Data as DataHelper;
 use Algolia\AlgoliaSearch\Helper\Entity\ProductHelper;
 use Magento\Backend\App\Action\Context;
-use Magento\Catalog\Model\ProductFactory;
+use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Framework\Controller\ResultFactory;
 use Magento\Store\Model\StoreManagerInterface;
 
@@ -18,8 +18,8 @@ class Save extends \Magento\Backend\App\Action
 {
     const MAX_SKUS = 10;
 
-    /** @var ProductFactory */
-    private $productFactory;
+    /** @var ProductRepositoryInterface */
+    private $productRepository;
 
     /** @var StoreManagerInterface */
     private $storeManager;
@@ -31,24 +31,25 @@ class Save extends \Magento\Backend\App\Action
     private $productHelper;
 
     /**
-     * @param Context               $context
-     * @param ProductFactory        $productFactory
+     * Save constructor.
+     * @param Context $context
+     * @param ProductRepositoryInterface $productRepository
      * @param StoreManagerInterface $storeManager
-     * @param DataHelper            $dataHelper
-     * @param ProductHelper         $productHelper
+     * @param DataHelper $dataHelper
+     * @param ProductHelper $productHelper
      */
     public function __construct(
         Context $context,
-        ProductFactory $productFactory,
+        ProductRepositoryInterface $productRepository,
         StoreManagerInterface $storeManager,
         DataHelper $dataHelper,
         ProductHelper $productHelper
     ) {
         parent::__construct($context);
-        $this->productFactory = $productFactory;
         $this->storeManager = $storeManager;
         $this->dataHelper = $dataHelper;
         $this->productHelper = $productHelper;
+        $this->productRepository = $productRepository;
     }
 
     /**
@@ -65,8 +66,6 @@ class Save extends \Magento\Backend\App\Action
         $skus = preg_split("/(,|\r\n|\n|\r)/", $this->getRequest()->getParam('skus'));
 
         $stores = $this->storeManager->getStores();
-        $websites = $this->storeManager->getWebsites();
-        $storeGroup = $this->storeManager->getGroups();
 
         foreach ($stores as $storeId => $storeData) {
             if ($this->dataHelper->isIndexingEnabled($storeId) === false) {
@@ -90,10 +89,9 @@ class Save extends \Magento\Backend\App\Action
         foreach ($skus as $sku) {
             $sku = trim($sku);
             try {
-                /** @var \Magento\Catalog\Model\Product $product */
-                $product = $this->productFactory->create();
-                $product->load($product->getIdBySku($sku));
 
+                /** @var \Magento\Catalog\Model\Product $product */
+                $product = $this->productRepository->get($sku);
                 if (! $product->getId()) {
                     throw new UnknownSkuException(__('Product with SKU "%1" was not found.', $sku));
                 }
@@ -101,69 +99,20 @@ class Save extends \Magento\Backend\App\Action
                 $this->checkAndReindex($product, $stores);
             } catch (UnknownSkuException $e) {
                 $this->messageManager->addExceptionMessage($e, $e->getMessage());
-            } catch (ProductDisabledException $e) {
-                $this->messageManager->addExceptionMessage(
-                    $e,
-                    __(
-                        'The product "%1" (%2) is disabled in store "%3 / %4 / %5".',
-                        [$e->getProduct()->getName(), $e->getProduct()->getSku(),
-                            $websites[$stores[$e->getStoreId()]->getWebsiteId()]->getName(),
-                            $storeGroup[$stores[$e->getStoreId()]->getStoreGroupId()]->getName(),
-                            $stores[$e->getStoreId()]->getName()]
-                    )
-                );
             } catch (ProductDeletedException $e) {
                 $this->messageManager->addExceptionMessage(
                     $e,
                     __(
-                        'The product "%1" (%2) is deleted from store "%3 / %4 / %5".',
-                        [$e->getProduct()->getName(), $e->getProduct()->getSku(),
-                            $websites[$stores[$e->getStoreId()]->getWebsiteId()]->getName(),
-                            $storeGroup[$stores[$e->getStoreId()]->getStoreGroupId()]->getName(),
-                            $stores[$e->getStoreId()]->getName()]
-                    )
-                );
-            } catch (ProductNotVisibleException $e) {
-                // If it's a simple product that is not visible, try to index its parent if it exists
-                if ($e->getProduct()->getTypeId() == 'simple') {
-                    $parentId = $this->productHelper->getParentProductIds([$e->getProduct()->getId()]);
-                    if (isset($parentId[0])) {
-                        $parentId = $parentId[0];
-                        /** @var \Magento\Catalog\Model\Product $parentProduct */
-                        $parentProduct = $this->productFactory->create();
-                        $parentProduct->load($parentId);
-                        $this->messageManager->addNoticeMessage(
-                            __(
-                                'The product "%1" (%2) is not visible but it has a parent product "%3" (%4).',
-                                [$e->getProduct()->getName(), $e->getProduct()->getSku(), $parentProduct->getName(),
-                                    $parentProduct->getSku()]
-                            )
-                        );
-
-                        $this->checkAndReindex($parentProduct, $stores);
-                        continue;
-                    }
-                }
-
-                $this->messageManager->addExceptionMessage(
-                    $e,
-                    __(
-                        'The product "%1" (%2) is not visible in store "%3 / %4 / %5".',
-                        [$e->getProduct()->getName(), $e->getProduct()->getSku(),
-                            $websites[$stores[$e->getStoreId()]->getWebsiteId()]->getName(),
-                            $storeGroup[$stores[$e->getStoreId()]->getStoreGroupId()]->getName(),
-                            $stores[$e->getStoreId()]->getName()]
+                        'The product "%1" (%2) is deleted.',
+                        [$e->getProduct()->getName(), $e->getProduct()->getSku()]
                     )
                 );
             } catch (ProductOutOfStockException $e) {
                 $this->messageManager->addExceptionMessage(
                     $e,
                     __(
-                        'The product "%1" (%2) is out of stock in store "%3 / %4 / %5".',
-                        [$e->getProduct()->getName(), $e->getProduct()->getSku(),
-                            $websites[$stores[$e->getStoreId()]->getWebsiteId()]->getName(),
-                            $storeGroup[$stores[$e->getStoreId()]->getStoreGroupId()]->getName(),
-                            $stores[$e->getStoreId()]->getName()]
+                        'The product "%1" (%2) is out of stock.',
+                        [$e->getProduct()->getName(), $e->getProduct()->getSku()]
                     )
                 );
             }
@@ -197,7 +146,60 @@ class Save extends \Magento\Backend\App\Action
 
                 continue;
             }
-            $this->dataHelper->canProductBeReindexed($product, $storeId);
+
+            try {
+                $product = $this->productRepository->get($product->getSku(), false, $storeId);
+                $this->dataHelper->canProductBeReindexed($product, $storeId);
+            } catch (ProductDisabledException $e) {
+                // Product status is a Website specific attribute
+                $this->messageManager->addErrorMessage(
+                    $e,
+                    __(
+                        'The product "%1" (%2) is disabled in store "%3 / %4 / %5".',
+                        [$e->getProduct()->getName(), $e->getProduct()->getSku(),
+                            $websites[$stores[$e->getStoreId()]->getWebsiteId()]->getName(),
+                            $storeGroup[$stores[$e->getStoreId()]->getStoreGroupId()]->getName(),
+                            $stores[$e->getStoreId()]->getName()]
+                    )
+                );
+
+                continue;
+
+            } catch (ProductNotVisibleException $e) {
+                // Product visibility is a Store specific attribute
+                // If it's a simple product that is not visible, try to index its parent if it exists
+                if ($e->getProduct()->getTypeId() == 'simple') {
+                    $parentId = $this->productHelper->getParentProductIds([$e->getProduct()->getId()]);
+                    if (isset($parentId[0])) {
+                        $parentId = $parentId[0];
+                        /** @var \Magento\Catalog\Model\Product $parentProduct */
+                        $parentProduct = $this->productRepository->getById($parentId, false, $e->getStoreId());
+                        $this->messageManager->addNoticeMessage(
+                            __(
+                                'The product "%1" (%2) is not visible but it has a parent product "%3" (%4).',
+                                [$e->getProduct()->getName(), $e->getProduct()->getSku(), $parentProduct->getName(),
+                                    $parentProduct->getSku()]
+                            )
+                        );
+
+                        $this->checkAndReindex($parentProduct, $stores);
+                        continue;
+                    }
+                } else {
+                    $this->messageManager->addErrorMessage(
+                        $e,
+                        __(
+                            'The product "%1" (%2) is not visible in store "%3 / %4 / %5".',
+                            [$e->getProduct()->getName(), $e->getProduct()->getSku(),
+                                $websites[$stores[$e->getStoreId()]->getWebsiteId()]->getName(),
+                                $storeGroup[$stores[$e->getStoreId()]->getStoreGroupId()]->getName(),
+                                $stores[$e->getStoreId()]->getName()]
+                        )
+                    );
+
+                    continue;
+                }
+            }
 
             $productIds = [$product->getId()];
             $productIds = array_merge($productIds, $this->productHelper->getParentProductIds($productIds));

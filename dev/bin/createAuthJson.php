5425f3877dc2e3391d7577fb588e44d75cbb6509
Jan Petr <honzik.petr@gmail.com>
2019-03-25 19:00:22 +0100

    Create Composer's auth file

--- dev/bin/createAuthJson.php
+++ dev/bin/createAuthJson.php
@@ -6,8 +6,15 @@ $json['http-basic']['repo.magento.com'] = [
 ];
 
 $dirname = dirname(getenv('AUTH_DIR'));
+
 if (!is_dir($dirname)) {
     mkdir($dirname, 0777, true);
 }
 
-file_put_contents(getenv('AUTH_DIR'), json_encode($json));
+if (file_put_contents(getenv('AUTH_DIR'), json_encode($json))) {
+    echo 'Auth.json file created in directory "' . $dirname . '"';
+    exit(0);
+}
+
+echo 'Error while creating auth.json file';
+exit(1);

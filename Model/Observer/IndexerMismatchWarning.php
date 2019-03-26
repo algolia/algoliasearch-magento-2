ce936992d06c1f217f04c4490b7854fa6595c3c2
Jan Petr <jan.petr@algolia.com>
2019-03-26 12:47:17 +0100

    Fix CS

--- Model/Observer/IndexerMismatchWarning.php
+++ Model/Observer/IndexerMismatchWarning.php
@@ -2,26 +2,27 @@
 
 namespace Algolia\AlgoliaSearch\Model\Observer;
 
-use Magento\Framework\Message\ManagerInterface;
-use Magento\Framework\Mview\View\StateInterface;
 use Magento\Catalog\Model\Indexer\Product\Price\Processor as PriceProcessor;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\Message\ManagerInterface;
+use Magento\Framework\Mview\View\StateInterface;
 
 class IndexerMismatchWarning implements ObserverInterface
 {
     /**
-     * @var \Magento\Framework\Message\ManagerInterface
+     * @var ManagerInterface
      */
     protected $messageManager;
 
     /**
-     * @var \Magento\Framework\Mview\View\StateInterface
+     * @var StateInterface
      */
     protected $mviewState;
 
     /**
      * IndexerWarning constructor.
+     *
      * @param StateInterface $mviewState
      * @param ManagerInterface $messageManager
      */
@@ -36,11 +37,10 @@ class IndexerMismatchWarning implements ObserverInterface
     public function execute(Observer $observer)
     {
         if (
-            $this->mviewState->loadByView('algolia_products')->getMode() == $this->mviewState::MODE_DISABLED
-            &&
+            $this->mviewState->loadByView('algolia_products')->getMode() == $this->mviewState::MODE_DISABLED &&
             $this->mviewState->loadByView(PriceProcessor::INDEXER_ID)->getMode() == $this->mviewState::MODE_ENABLED
         ) {
-            $this->messageManager->addWarningMessage(__('Warning; Algolia Product indexer is set to Update on Save but Catalog Product Price indexer is set to Update on Schedule. This might cause problems with the pricing being synced to Algolia. Please set both to the same setting.'));
+            $this->messageManager->addWarningMessage(__('Warning; Algolia Product indexer is set to "Update on Save", but Catalog Product Price indexer is set to "Update on Schedule". This might cause problems with the pricing being synced to Algolia. Please set both to the same setting.'));
         }
     }
 }

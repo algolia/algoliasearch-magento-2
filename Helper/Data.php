2082664fc84f7b1eafd8f5c8085c591a0da35581
Volodymyr Malyk <vladimir@convert.no>
2019-06-27 16:06:31 +0300

    [highlight-results-undefined-fix] Check hit._highlightResult it present in hit. Prevent clear attributesToHighlight on re-save index.

--- Helper/Data.php
+++ Helper/Data.php
@@ -113,7 +113,6 @@ class Data
         $params = [
             'hitsPerPage'            => $numberOfResults, // retrieve all the hits (hard limit is 1000)
             'attributesToRetrieve'   => 'objectID',
-            'attributesToHighlight'  => '',
             'attributesToSnippet'    => '',
             'numericFilters'         => 'visibility_search=1',
             'removeWordsIfNoResults' => $this->configHelper->getRemoveWordsIfNoResult($storeId),

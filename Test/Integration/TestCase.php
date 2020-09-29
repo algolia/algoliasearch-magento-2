48439784b514f49b07b8736a9d217d9bf3dfa459
Betty Suravech <bsuravech@gmail.com>
2020-09-29 17:18:37 +0200

    tear down void

--- Test/Integration/TestCase.php
+++ Test/Integration/TestCase.php
@@ -45,7 +45,7 @@ abstract class TestCase extends \TC
         $this->bootstrap();
     }
 
-    public function tearDown()
+    public function tearDown(): void
     {
         $this->clearIndices();
     }

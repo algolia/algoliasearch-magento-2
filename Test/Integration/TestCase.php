8f415d1b91b3d891a191e5a1130447762dcdbbbd
Ratikant Singh <ratikant.singh@algolia.com>
2022-04-28 18:12:45 +0530

    integration test fix

--- Test/Integration/TestCase.php
+++ Test/Integration/TestCase.php
@@ -116,9 +116,9 @@ abstract class TestCase extends \TC
 
         $this->configHelper = $config = $this->getObjectManager()->create(ConfigHelper::class);
 
-        $this->setConfig('algoliasearch_credentials/credentials/application_id', 'KR65MCCV6D');
-        $this->setConfig('algoliasearch_credentials/credentials/search_only_api_key', 'f26e954de7ecb2120e501224f0b05eac');
-        $this->setConfig('algoliasearch_credentials/credentials/api_key', '9e0b18704026084879e0aa5d9d0ec239');
+        $this->setConfig('algoliasearch_credentials/credentials/application_id', getenv('ALGOLIA_APPLICATION_ID'));
+        $this->setConfig('algoliasearch_credentials/credentials/search_only_api_key', getenv('ALGOLIA_SEARCH_KEY_1') ?: getenv('ALGOLIA_SEARCH_API_KEY'));
+        $this->setConfig('algoliasearch_credentials/credentials/api_key', getenv('ALGOLIA_API_KEY'));
 
         $this->indexPrefix =  'magento2_' . date('Y-m-d_H:i:s') . '_' . (getenv('INDEX_PREFIX') ?: 'circleci_');
         $this->setConfig('algoliasearch_credentials/credentials/index_prefix', $this->indexPrefix);

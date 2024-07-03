d09985943a0fe38e9ab4c6164323f08c09417bec
Damien Couchez <damien.couchez@gmail.com>
2024-07-03 17:32:43 +0200

    MAGE-926: port changes from PR #1506

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -20,11 +20,11 @@ var config = {
         }
     },
     paths : {
-        'algoliaBundle'   : 'Algolia_AlgoliaSearch/internals/algoliaBundle.min',
-        'algoliaAnalytics': 'Algolia_AlgoliaSearch/internals/search-insights',
-        'recommend'       : 'Algolia_AlgoliaSearch/internals/recommend.min',
-        'recommendJs'     : 'Algolia_AlgoliaSearch/internals/recommend-js.min',
-        'rangeSlider'     : 'Algolia_AlgoliaSearch/navigation/range-slider-widget',
+        'algoliaBundle'   : 'Algolia_AlgoliaSearch/js/internals/algoliaBundle.min',
+        'algoliaAnalytics': 'Algolia_AlgoliaSearch/js/internals/search-insights',
+        'recommend'       : 'Algolia_AlgoliaSearch/js/internals/recommend.min',
+        'recommendJs'     : 'Algolia_AlgoliaSearch/js/internals/recommend-js.min',
+        'rangeSlider'     : 'Algolia_AlgoliaSearch/js/navigation/range-slider-widget',
     },
     deps  : [
         'algoliaInstantSearch',

a684e49cd0c37633b0a413a02dcc7191009ba83b
Thomas Klein <thomasklein876@gmail.com>
2024-08-21 17:52:42 +0200

    Prevent unexisting created_at key

--- Helper/AlgoliaHelper.php
+++ Helper/AlgoliaHelper.php
@@ -593,7 +593,7 @@ class AlgoliaHelper extends AbstractHelper
         foreach ($objects as $key => &$object) {
             $object['algoliaLastUpdateAtCET'] = $currentCET;
             // Convert created_at to UTC timestamp
-            $object['created_at'] = strtotime($object['created_at']);
+            $object['created_at'] = strtotime($object['created_at'] ?? 'now');
 
             $previousObject = $object;
 

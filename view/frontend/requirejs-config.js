8247f9a481e82dcae1dc24a484a43faaccd6bf6c
Eric Wright <eric.wright@algolia.com>
2024-09-03 17:33:32 -0400

    MAGE-999 Update HTML template paths to align with new naming convention for unpacked algoliaBundle (legacy paths retained for backward compat and deprecated)

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -9,38 +9,46 @@ const config = {
             'algoliaHooks'        : 'Algolia_AlgoliaSearch/js/hooks',
 
             // Autocomplete templates
-            'productsHtml'   : 'Algolia_AlgoliaSearch/js/template/autocomplete/products',
-            'pagesHtml'      : 'Algolia_AlgoliaSearch/js/template/autocomplete/pages',
-            'categoriesHtml' : 'Algolia_AlgoliaSearch/js/template/autocomplete/categories',
-            'suggestionsHtml': 'Algolia_AlgoliaSearch/js/template/autocomplete/suggestions',
-            'additionalHtml' : 'Algolia_AlgoliaSearch/js/template/autocomplete/additional-section',
+            'algoliaAutocompleteProductsHtml'   : 'Algolia_AlgoliaSearch/js/template/autocomplete/products',
+            'algoliaAutocompletePagesHtml'      : 'Algolia_AlgoliaSearch/js/template/autocomplete/pages',
+            'algoliaAutocompleteCategoriesHtml' : 'Algolia_AlgoliaSearch/js/template/autocomplete/categories',
+            'algoliaAutocompleteSuggestionsHtml': 'Algolia_AlgoliaSearch/js/template/autocomplete/suggestions',
+            'algoliaAutocompleteAdditionalHtml' : 'Algolia_AlgoliaSearch/js/template/autocomplete/additional-section',
 
             // Recommend templates
-            'recommendProductsHtml': 'Algolia_AlgoliaSearch/js/template/recommend/products',
+            'algoliaRecommendProductsHtml': 'Algolia_AlgoliaSearch/js/template/recommend/products',
 
             // Unbundling
             'algoliaTemplateEngine': 'Algolia_AlgoliaSearch/js/internals/template-engine'
         }
     },
     paths : {
+        // Core Search UI libs
         'algoliaSearchLib'                : 'Algolia_AlgoliaSearch/js/lib/algolia-search.min',
         'algoliaInstantSearchLib'         : 'Algolia_AlgoliaSearch/js/lib/algolia-instantsearch.min',
         'algoliaAutocompleteLib'          : 'Algolia_AlgoliaSearch/js/lib/algolia-autocomplete.min',
         'algoliaAnalytics'                : 'Algolia_AlgoliaSearch/js/internals/search-insights',
-        'algoliaQuerySuggestionsPluginLib': 'Algolia_AlgoliaSearch/js/lib/query-suggestions-plugin.min',
         'algoliaRecommendLib'             : 'Algolia_AlgoliaSearch/js/lib/recommend.min',
         'algoliaRecommendJsLib'           : 'Algolia_AlgoliaSearch/js/lib/recommend-js.min',
 
-        'rangeSlider': 'Algolia_AlgoliaSearch/js/navigation/range-slider-widget',
+        // Autocomplete plugins
+        'algoliaQuerySuggestionsPluginLib': 'Algolia_AlgoliaSearch/js/lib/query-suggestions-plugin.min',
 
         // Legacy
         'algoliaMustacheLib': 'Algolia_AlgoliaSearch/js/lib/mustache.min',
         'algoliaHoganLib'   : 'Algolia_AlgoliaSearch/js/lib/hogan.min',
 
         // DEPRECATED
-        'algoliaBundle'     : 'Algolia_AlgoliaSearch/js/internals/algoliaBundle.min',
-        'recommend'         : 'Algolia_AlgoliaSearch/js/lib/recommend.min',
-        'recommendJs'       : 'Algolia_AlgoliaSearch/js/lib/recommend-js.min',
+        'algoliaBundle'        : 'Algolia_AlgoliaSearch/js/internals/algoliaBundle.min',
+        'rangeSlider'          : 'Algolia_AlgoliaSearch/js/navigation/range-slider-widget',
+        'recommend'            : 'Algolia_AlgoliaSearch/js/lib/recommend.min',
+        'recommendJs'          : 'Algolia_AlgoliaSearch/js/lib/recommend-js.min',
+        'productsHtml'         : 'Algolia_AlgoliaSearch/js/template/autocomplete/products',
+        'pagesHtml'            : 'Algolia_AlgoliaSearch/js/template/autocomplete/pages',
+        'categoriesHtml'       : 'Algolia_AlgoliaSearch/js/template/autocomplete/categories',
+        'suggestionsHtml'      : 'Algolia_AlgoliaSearch/js/template/autocomplete/suggestions',
+        'additionalHtml'       : 'Algolia_AlgoliaSearch/js/template/autocomplete/additional-section',
+        'recommendProductsHtml': 'Algolia_AlgoliaSearch/js/template/recommend/products',
     },
     deps  : [
         'algoliaInstantSearch',

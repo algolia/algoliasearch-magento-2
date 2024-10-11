1bf61517349d49267d418ec0fdc95f7385f93269
Mohammad Rahman <mohammad.rahman@algolia.com>
2024-10-11 12:47:27 -0400

    MAGE-1050: code updated as per code review suggestions.

--- Test/Integration/Search/SearchTest.php
+++ Test/Integration/Search/SearchTest.php
@@ -5,6 +5,7 @@ namespace Algolia\AlgoliaSearch\Test\Integration\Search;
 use Algolia\AlgoliaSearch\Helper\Data;
 use Algolia\AlgoliaSearch\Model\Indexer\Product;
 use Algolia\AlgoliaSearch\Test\Integration\TestCase;
+use Magento\Framework\Exception\NoSuchEntityException;
 
 class SearchTest extends TestCase
 {
@@ -27,15 +28,68 @@ class SearchTest extends TestCase
 
     public function testSearch()
     {
-        list($results, $totalHits, $facetsFromAlgolia) = $this->helper->getSearchResult('', 1);
-        $this->assertNotEmpty($results);
+        $query = 'bag';
+        $results = $this->search($query);
+        $result = $this->getFirstResult($results);
+        // Search returns result
+        $this->assertNotEmpty($result, "Query didn't bring result");
+
+        $product = $this->objectManager->create(\Magento\Catalog\Model\Product::class);
+        $product->load($result['entity_id']);
+        // Result exists in DB
+        $this->assertNotEmpty($product->getName(), "Query result item couldn't find in the DB");
+        // Query word exists title
+        $this->assertStringContainsString($query, strtolower($product->getName()), "Query word doesn't exist in product name");
+    }
+
+    public function testSearchBySku()
+    {
+        $sku = "24-MB01";
+        $results = $this->search($sku);
+        $result = $this->getFirstResult($results);
+        // Search by SKU returns result
+        $this->assertNotEmpty($result, "SKU search didn't bring result");
+
+        $product = $this->objectManager->create(\Magento\Catalog\Model\Product::class);
+        $product->load($result['entity_id']);
+        // Result exists in DB
+        $this->assertNotEmpty($product->getSku(), "SKU search result item couldn't find in the DB");
+        // Query word exists title
+        $this->assertEquals($sku, $product->getSku(), "Query SKU doesn't match with product SKU");
     }
 
     public function testCategorySearch()
     {
-        list($results, $totalHits, $facetsFromAlgolia) = $this->helper->getSearchResult('', 1, [
+        // Get products by categoryId
+        $results = $this->search('', 1, [
             'facetFilters' => ['categoryIds:' . $this->assertValues->expectedCategory]
         ]);
-        $this->assertNotEmpty($results);
+        $result = $this->getFirstResult($results);
+        // Category filter returns result
+        $this->assertNotEmpty($result, "Category filter didn't return result");
+    }
+
+    /**
+     * @param array $results
+     * @return array
+     */
+    protected function getFirstResult(array $results): array
+    {
+        list($results, $totalHits, $facetsFromAlgolia) = $results;
+        return array_shift($results);
+    }
+
+    /**
+     * @param string $query
+     * @param int $storeId
+     * @return array
+     */
+    protected function search(string $query = '', int $storeId = 1, array $params = []): array
+    {
+        try {
+            return $this->helper->getSearchResult($query, $storeId, $params);
+        } catch (NoSuchEntityException $e) {
+            return [];
+        }
     }
 }

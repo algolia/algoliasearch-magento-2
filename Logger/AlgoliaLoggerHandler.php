f24a6bcbeb0a1e145c3fe4b5b83c327b1fd1ae9c
Eric Wright <eric.wright@algolia.com>
2024-11-11 18:33:05 -0500

    MAGE-1104 Only send errors to system.log

--- Logger/AlgoliaLoggerHandler.php
+++ Logger/AlgoliaLoggerHandler.php
@@ -5,7 +5,7 @@ namespace Algolia\AlgoliaSearch\Logger;
 use Magento\Framework\Logger\Handler\Base;
 use Monolog\Logger;
 
-class LoggerHandler extends Base
+class AlgoliaLoggerHandler extends Base
 {
     protected $fileName = '/var/log/algolia.log';
     protected $loggerType = Logger::DEBUG; // Default

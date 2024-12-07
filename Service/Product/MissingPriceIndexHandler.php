1672f59a700861ba68665b066707ce1b7b8452f2
Eric Wright <eric.wright@algolia.com>
2024-12-07 18:51:10 -0500

    MAGE-1122 Add flexible retrieval of price reindex ids

--- Service/Product/MissingPriceIndexHandler.php
+++ Service/Product/MissingPriceIndexHandler.php
@@ -2,12 +2,19 @@
 
 namespace Algolia\AlgoliaSearch\Service\Product;
 
+use Magento\Framework\DB\Select;
+use Magento\Catalog\Model\ResourceModel\Product\Collection as ProductCollection;
 use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory;
 use Magento\Framework\Indexer\IndexerRegistry;
 use Magento\Framework\Indexer\IndexerInterface;
+use Zend_Db_Select;
 
 class MissingPriceIndexHandler
 {
+    public const PRICE_INDEX_TABLE = 'catalog_product_index_price';
+    public const PRICE_INDEX_TABLE_ALIAS = 'price_index';
+    public const MAIN_TABLE_ALIAS = 'e';
+
     protected IndexerInterface $indexer;
     public function __construct(
         protected CollectionFactory $productCollectionFactory,
@@ -18,12 +25,12 @@ class MissingPriceIndexHandler
     }
 
     /**
-     * @param array $productIds
-     * @return int[] Array of product IDs that were reindexed by this repair operation
+     * @param string[]|ProductCollection $products
+     * @return string[] Array of product IDs that were reindexed by this repair operation
      */
-    public function refreshPriceIndex(array $productIds): array
+    public function refreshPriceIndex(array|ProductCollection $products): array
     {
-        $reindexIds = $this->getProductIdsToReindex($productIds);
+        $reindexIds = $this->getProductIdsToReindex($products);
         if (empty($reindexIds)) {
             return [];
         }
@@ -34,16 +41,25 @@ class MissingPriceIndexHandler
     }
 
     /**
-     * @param int[] $productIds
-     * @return int[]
+     * @param string[]|ProductCollection $products
+     * @return string[]
      */
-    protected function getProductIdsToReindex(array $productIds): array
+    protected function getProductIdsToReindex(array|ProductCollection $products): array
     {
+        $productIds = $products instanceof ProductCollection
+            ? $this->getExpandedProductCollectionIds($products)
+            : $products;
+
         $state = $this->indexer->getState()->getStatus();
         if ($state === \Magento\Framework\Indexer\StateInterface::STATUS_INVALID) {
             return $productIds;
         }
 
+        return $this->filterProductIds($productIds);
+    }
+
+    protected function filterProductIds(array $productIds): array
+    {
         $collection = $this->productCollectionFactory->create();
 
         $collection->addAttributeToSelect(['name', 'price']);
@@ -56,8 +72,85 @@ class MissingPriceIndexHandler
 
         $collection->getSelect()
             ->where('price_index.entity_id IS NULL')
-            ->where('entity_id IN (?)', $productIds);
+            ->where('e.entity_id IN (?)', $productIds);
 
         return $collection->getAllIds();
+    }
+
+    protected function getExpandedProductCollectionIds(ProductCollection $collection): array
+    {
+        $expandedCollection = clone $collection;
+
+        $select = $expandedCollection->getSelect();
+
+        $joins = $select->getPart(Zend_Db_Select::FROM);
+
+        $priceIndexJoin = $this->getPriceIndexJoinAlias($joins);
+
+        if (!$priceIndexJoin) {
+            // nothing to do here - keep calm and carry on
+            return [];
+        }
+
+        $modifyJoin = &$joins[$priceIndexJoin];
+        $modifyJoin['joinType'] = Zend_Db_Select::LEFT_JOIN;
+
+        $this->rebuildJoins($select, $joins);
+
+        return $expandedCollection->getAllIds();
+    }
+
+    protected function rebuildJoins(Select $select, array $joins): void
+    {
+        $select->reset(Zend_Db_Select::FROM);
+        foreach ($joins as $alias => $joinData) {
+            if ($joinData['joinType'] === Zend_Db_Select::FROM) {
+                $select->from([$alias => $joinData['tableName']]);
+            } elseif ($joinData['joinType'] === Zend_Db_Select::LEFT_JOIN) {
+                $select->joinLeft(
+                    [$alias => $joinData['tableName']],
+                    $joinData['joinCondition'],
+                    [],
+                    $joinData['schema']
+                );
+            } else {
+                $select->join(
+                    [$alias => $joinData['tableName']],
+                    $joinData['joinCondition'],
+                    [],
+                    $joinData['schema']
+                );
+            }
+            $sql = $select->__toString();
+        }
+    }
+
+    private function inspectJoins(array $joins): void {
+        foreach ($joins as $alias => $joinData) {
+            echo "Table Alias: $alias\n";
+            echo "Table Name: {$joinData['tableName']}\n";
+            echo "Join Type: {$joinData['joinType']}\n";
+            echo "Join Condition: " . ($joinData['joinCondition'] ?? 'N/A') . "\n\n";
+        }
+    }
+
+    /**
+     * @param array<string, array> $joins
+     * @return string
+     */
+    protected function getPriceIndexJoinAlias(array $joins): string
+    {
+        if (isset($joins[self::PRICE_INDEX_TABLE_ALIAS])) {
+            return self::PRICE_INDEX_TABLE_ALIAS;
+        }
+        else {
+            foreach ($joins as $alias => $joinData) {
+                if ($joinData['tableName'] === self::PRICE_INDEX_TABLE) {
+                    return $alias;
+                }
+            }
+        }
+
+        return "";
     }
 }

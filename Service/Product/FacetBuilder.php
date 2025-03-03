ee7b87d0bb4a57bf057dcd63c2c2970c49536d7e
Eric Wright <eric.wright@algolia.com>
2025-03-03 16:48:50 -0500

    MAGE-973 Introduce raw facet meta data cache for both attributesForFaceting and renderingContent

--- Service/Product/FacetBuilder.php
+++ Service/Product/FacetBuilder.php
@@ -13,6 +13,14 @@ use Magento\Store\Model\StoreManagerInterface;
 class FacetBuilder
 {
     public const FACET_ATTRIBUTE_PRICE = 'price';
+    public const FACET_ATTRIBUTE_CATEGORIES = 'categories';
+    public const FACET_ATTRIBUTES_CATEGORY_ID = 'categoryIds';
+
+    public const FACET_KEY_ATTRIBUTE_NAME = 'attribute';
+    public const FACET_KEY_SEARCHABLE = 'searchable';
+
+    // Local raw facet cache, indexed by $storeId
+    protected array $facets = [];
 
     public function __construct(
         protected ConfigHelper                            $configHelper,
@@ -26,11 +34,130 @@ class FacetBuilder
     /**
      * @param int $storeId
      * @return array<string, array>|null
+     * @throws LocalizedException
+     * @throws NoSuchEntityException
      */
     public function getRenderingContent(int $storeId): ?array
     {
-        return null;
+        $facets = $this->getRawFacets($storeId);
+        if (empty($facets)) {
+            return null;
+        }
+        $attributes = $this->extractFacetAttributeNames($facets);
+        return [
+            'facetOrdering' => [
+                'facets' => [
+                    'order' => $attributes
+                ],
+                'values' => $this->getRenderingContentValues($attributes)
+            ],
+        ];
     }
+
+    /**
+     * @param array<array<string, mixed>> $facets
+     * @return string[]
+     */
+    protected function extractFacetAttributeNames(array $facets): array
+    {
+        return array_map(
+            function($facet) {
+                return $facet[self::FACET_KEY_ATTRIBUTE_NAME];
+            },
+            $facets
+        );
+    }
+
+    /**
+     * @param string[] $facets
+     * @return array<string, array>
+     */
+    protected function getRenderingContentValues(array $facets): array
+    {
+        return array_combine(
+            array_map(
+                function(string $attribute) {
+                    if ($attribute === self::FACET_ATTRIBUTE_CATEGORIES) {
+                        $attribute = self::FACET_ATTRIBUTE_CATEGORIES . '.level0';
+                    }
+                    return $attribute;
+                },
+                $facets
+            ),
+            array_fill(0, count($facets), [ 'sortRemainingBy' => 'alpha' ])
+        );
+    }
+
+    /**
+     * @param string $attribute
+     * @param bool $searchable
+     * @return array<string, string|int>
+     */
+    protected function getRawFacet(string $attribute, bool $searchable = false): array
+    {
+        return [
+            self::FACET_KEY_ATTRIBUTE_NAME => $attribute,
+            self::FACET_KEY_SEARCHABLE => $searchable ? 1 : 0,
+        ];
+    }
+
+    /**
+     * Generates common data to be used for both attributesForFaceting and renderingContent
+     * @return array<array<string, mixed>>
+     * @throws NoSuchEntityException
+     * @throws LocalizedException
+     */
+    protected function getRawFacets(int $storeId): array
+    {
+        if (isset($this->facets[$storeId])) {
+            return $this->facets[$storeId];
+        }
+
+        $rawFacets = [];
+        $configFacets = $this->configHelper->getFacets($storeId);
+        foreach ($configFacets as $configFacet) {
+            if ($configFacet[self::FACET_KEY_ATTRIBUTE_NAME] === self::FACET_ATTRIBUTE_PRICE) {
+                $rawFacets = array_merge($rawFacets, array_map(
+                    function(string $attribute) {
+                        return $this->getRawFacet($attribute);
+                    },
+                    $this->getPricingAttributes($storeId)
+                ));
+            } else {
+                $rawFacets[] = $configFacet;
+            }
+        }
+
+        $this->facets[$storeId] = $this->addCategoryFacets($storeId, $rawFacets);
+        return $this->facets[$storeId];
+    }
+
+    /**
+     * @param int $storeId
+     * @param array<array<string, mixed>> $facets
+     * @return array<array<string, mixed>>
+     */
+    protected function addCategoryFacets(int $storeId, array $facets): array
+    {
+        if ($this->configHelper->replaceCategories($storeId)
+            && !array_filter($facets, function($facet) {
+                return $facet['attribute'] === self::FACET_ATTRIBUTE_CATEGORIES;
+            })
+        ) {
+            $facets[] = $this->getRawFacet(self::FACET_ATTRIBUTE_CATEGORIES);
+        }
+
+        // Added for legacy merchandising features
+        $facets[] = $this->getRawFacet(self::FACET_ATTRIBUTES_CATEGORY_ID);
+
+        if ($this->configHelper->isVisualMerchEnabled($storeId)) {
+            // Must be searchable per https://www.algolia.com/doc/guides/solutions/ecommerce/browse/tutorials/category-pages/#configure-your-index
+            $facets[] = $this->getRawFacet($this->configHelper->getCategoryPageIdAttributeName($storeId), true);
+        }
+
+        return $facets;
+    }
+
 
     /**
      * @param int $storeId
@@ -40,17 +167,14 @@ class FacetBuilder
      */
     public function getAttributesForFaceting(int $storeId): array
     {
+
         $attributesForFaceting = [];
-        $facets = $this->configHelper->getFacets($storeId);
+        $facets = $this->getRawFacets($storeId);
         foreach ($facets as $facet) {
-            if ($facet['attribute'] === self::FACET_ATTRIBUTE_PRICE) {
-                $attributesForFaceting = array_merge($attributesForFaceting, $this->getPricingAttributes($storeId));
-            } else {
-                $attributesForFaceting[] = $this->decorateAttributeForFaceting($facet);
-            }
+            $attributesForFaceting[] = $this->decorateAttributeForFaceting($facet);
         }
 
-        return $this->addCategoryAttributes($storeId, $attributesForFaceting);
+        return $attributesForFaceting;
     }
 
     /**
@@ -64,7 +188,7 @@ class FacetBuilder
         $pricingAttributes = [];
         $currencies = $this->currencyManager->getConfigAllowCurrencies();
         foreach ($currencies as $currencyCode) {
-            $pricingAttributes[] = 'price.' . $currencyCode . '.default';
+            $pricingAttributes[] = self::FACET_ATTRIBUTE_PRICE . '.' . $currencyCode . '.default';
             $pricingAttributes = array_merge($pricingAttributes, $this->getGroupPricingAttributes($storeId, $currencyCode));
         }
 
@@ -89,7 +213,7 @@ class FacetBuilder
                 if (in_array($websiteId, $excludedWebsites)) {
                     continue;
                 }
-                $groupPricingAttributes[] = 'price.' . $currencyCode . '.group_' . $groupId;
+                $groupPricingAttributes[] = self::FACET_ATTRIBUTE_PRICE . '.' . $currencyCode . '.group_' . $groupId;
             }
         }
         return $groupPricingAttributes;
@@ -97,15 +221,15 @@ class FacetBuilder
 
 
     /**
-     * @param array $facet
+     * @param array<string, string|int> $facet
      * @return string
      */
     protected function decorateAttributeForFaceting(array $facet): string {
-        $attribute = $facet['attribute'];
-        if (array_key_exists('searchable', $facet)) {
-            if ($facet['searchable'] === '1') {
+        $attribute = $facet[self::FACET_KEY_ATTRIBUTE_NAME];
+        if (array_key_exists(self::FACET_KEY_SEARCHABLE, $facet)) {
+            if ($facet[self::FACET_KEY_SEARCHABLE] === '1') {
                 $attribute = 'searchable(' . $attribute . ')';
-            } elseif ($facet['searchable'] === '3') {
+            } elseif ($facet[self::FACET_KEY_SEARCHABLE] === '3') {
                 $attribute = 'filterOnly(' . $attribute . ')';
             }
         }
@@ -120,7 +244,7 @@ class FacetBuilder
     protected function addCategoryAttributes(int $storeId, array $attributesForFaceting): array
     {
         if ($this->configHelper->replaceCategories($storeId) && !in_array('categories', $attributesForFaceting, true)) {
-            $attributesForFaceting[] = 'categories';
+            $attributesForFaceting[] = self::FACET_ATTRIBUTE_CATEGORIES;
         }
 
         // Added for legacy merchandising features

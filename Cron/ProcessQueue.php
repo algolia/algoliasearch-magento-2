2ce0361c6a9d7405ba8ae4cf0d6d84151d278ffb
Damien Couchez <damien.couchez@gmail.com>
2025-04-08 16:42:48 +0200

    MAGE-1228: added queue processing logic to the cron

--- Cron/ProcessQueue.php
+++ Cron/ProcessQueue.php
@@ -2,10 +2,30 @@
 
 namespace Algolia\AlgoliaSearch\Cron;
 
+use Algolia\AlgoliaSearch\Helper\ConfigHelper;
+use Algolia\AlgoliaSearch\Model\Queue;
+use Algolia\AlgoliaSearch\Service\AlgoliaCredentialsManager;
+
 class ProcessQueue
 {
+    public function __construct(
+        protected ConfigHelper $configHelper,
+        protected Queue $queue,
+        protected AlgoliaCredentialsManager $algoliaCredentialsManager
+    ) {}
+
     public function execute()
     {
+        if (!$this->configHelper->isQueueIndexerEnabled()) {
+            return;
+        }
 
+        if (!$this->algoliaCredentialsManager->checkCredentialsWithSearchOnlyAPIKey()) {
+            $this->algoliaCredentialsManager->displayErrorMessage(self::class);
+
+            return;
+        }
+
+        $this->queue->runCron();
     }
 }

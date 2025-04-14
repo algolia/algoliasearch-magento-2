99462cb3fd40469771fb5cb60a6517b7cbd2e963
Damien Couchez <damien.couchez@gmail.com>
2025-04-14 15:12:57 +0200

    Update Model/Config/EnableQueueComment.php
    Co-authored-by: Eric Wright <eric.wright@algolia.com>

--- Model/Config/EnableQueueComment.php
+++ Model/Config/EnableQueueComment.php
@@ -15,7 +15,7 @@ class EnableQueueComment implements CommentInterface
     {
         $url = $this->urlInterface->getUrl('algolia_algoliasearch/queue/index');
 
-        return 'If enabled, all indexing operations (add, remove & update operations) will be done asynchronously using the CRON mechanism.<br><br/> Go to <a href="' . $url . '">Indexing Queue</a>. <br/>
+        return 'If enabled, all indexing operations (add, remove & update operations) will be done asynchronously using the cron mechanism.<br><br/> Go to <a href="' . $url . '">Indexing Queue</a>. <br/>
 <span class="algolia-config-warning">&#9888;</span> Enabling this option is recommended in production or if your store has a lot of products. ';
     }
 }

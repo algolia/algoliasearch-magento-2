7f7804871404a09217bd17d8f59341500c596300
Damien Couchez <damien.couchez@gmail.com>
2025-05-20 13:34:02 +0200

    MAGE-1260: removed usage of AlgoliaHelper from SaveSettings

--- Model/Observer/SaveSettings.php
+++ Model/Observer/SaveSettings.php
@@ -3,7 +3,6 @@
 namespace Algolia\AlgoliaSearch\Model\Observer;
 
 use Algolia\AlgoliaSearch\Exceptions\AlgoliaException;
-use Algolia\AlgoliaSearch\Helper\AlgoliaHelper;
 use Algolia\AlgoliaSearch\Helper\Data;
 use Algolia\AlgoliaSearch\Helper\Entity\ProductHelper;
 use Algolia\AlgoliaSearch\Model\IndicesConfigurator;
@@ -13,47 +12,12 @@ use Magento\Store\Model\StoreManagerInterface;
 
 class SaveSettings implements ObserverInterface
 {
-    /** @var StoreManagerInterface */
-    protected $storeManager;
-
-    /** @var IndicesConfigurator */
-    protected $indicesConfigurator;
-
-    /**
-     * @var AlgoliaHelper
-     */
-    protected $algoliaHelper;
-
-    /**
-     * @var Data
-     */
-    protected $helper;
-
-    /**
-     * @var ProductHelper
-     */
-    protected $productHelper;
-
-    /**
-     * @param StoreManagerInterface $storeManager
-     * @param IndicesConfigurator $indicesConfigurator
-     * @param AlgoliaHelper $algoliaHelper
-     * @param Data $helper
-     * @param ProductHelper $productHelper
-     */
     public function __construct(
-        StoreManagerInterface $storeManager,
-        IndicesConfigurator $indicesConfigurator,
-        AlgoliaHelper $algoliaHelper,
-        Data $helper,
-        ProductHelper $productHelper
-    ) {
-        $this->storeManager = $storeManager;
-        $this->indicesConfigurator = $indicesConfigurator;
-        $this->algoliaHelper = $algoliaHelper;
-        $this->helper = $helper;
-        $this->productHelper = $productHelper;
-    }
+        protected StoreManagerInterface $storeManager,
+        protected IndicesConfigurator $indicesConfigurator,
+        protected Data $helper,
+        protected ProductHelper $productHelper
+    ){}
 
     /**
      * @param Observer $observer

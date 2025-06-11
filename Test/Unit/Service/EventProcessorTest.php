886031590eae7a0acc27243b30c5f61da65e3370
Damien Couchez <damien.couchez@gmail.com>
2025-06-11 16:18:03 +0200

    MAGE-1205: handle tax config

--- Test/Unit/Service/EventProcessorTest.php
+++ Test/Unit/Service/EventProcessorTest.php
@@ -5,11 +5,14 @@ namespace Algolia\AlgoliaSearch\Test\Unit\Service;
 use Algolia\AlgoliaSearch\Api\InsightsClient;
 use Algolia\AlgoliaSearch\Service\Insights\EventProcessor;
 use Magento\Sales\Model\Order\Item as OrderItem;
+use Magento\Store\Model\Store;
 use Magento\Store\Model\StoreManagerInterface;
+use Magento\Tax\Model\Config as TaxConfig;
 use PHPUnit\Framework\TestCase;
 
 class EventProcessorTest extends TestCase
 {
+    protected ?TaxConfig $taxConfig;
     protected ?InsightsClient $client;
     protected ?string $userToken;
     protected ?string $authenticatedUserToken;
@@ -23,7 +26,13 @@ class EventProcessorTest extends TestCase
         $this->authenticatedUserToken = 'authenticated-foo';
         $this->storeManager = $this->createMock(StoreManagerInterface::class);
 
+        $store = $this->createMock(Store::class);
+        $store->method('getId')->willReturn(1);
+        $this->storeManager->method('getStore')->willReturn($store);
+        $this->taxConfig =  $this->createMock(TaxConfig::class);
+
         $this->eventProcessor = new EventProcessorTestable(
+            $this->taxConfig,
             $this->client,
             $this->userToken,
             $this->authenticatedUserToken,
@@ -34,8 +43,10 @@ class EventProcessorTest extends TestCase
     /**
      * @dataProvider orderItemsProvider
      */
-    public function testObjectDataForPurchase($orderItemsData, $expectedResult, $expectedTotalRevenue): void
+    public function testObjectDataForPurchase($priceIncludesTax, $orderItemsData, $expectedResult, $expectedTotalRevenue): void
     {
+        $this->taxConfig->method('priceIncludesTax')->willReturn($priceIncludesTax);
+
         $orderItems = [];
 
         foreach ($orderItemsData as $orderItemData) {
@@ -61,8 +72,10 @@ class EventProcessorTest extends TestCase
     {
         return [
             [ // One item
+                'priceIncludesTax' => true,
                 'orderItemsData' => [
                     [
+                        'getPrice' => 32.00,
                         'getPriceInclTax' => 32.00,
                         'getOriginalPrice' => 32.00,
                         'getDiscountAmount' => 0.00,
@@ -78,9 +91,31 @@ class EventProcessorTest extends TestCase
                 ],
                 'expectedTotalRevenue' => 32.00
             ],
-            [ // One item with discount
+            [ // One item (tax excluded)
+                'priceIncludesTax' => false,
                 'orderItemsData' => [
                     [
+                        'getPrice' => 25.00,
+                        'getPriceInclTax' => 32.00,
+                        'getOriginalPrice' => 25.00,
+                        'getDiscountAmount' => 0.00,
+                        'getQtyOrdered' => 1,
+                    ]
+                ],
+                'expectedResult' => [
+                    [
+                        'price' => 25.00,
+                        'discount' => 0.00,
+                        'quantity' => 1,
+                    ]
+                ],
+                'expectedTotalRevenue' => 25.00
+            ],
+            [ // One item with discount
+                'priceIncludesTax' => true,
+                'orderItemsData' => [
+                    [
+                        'getPrice' => 32.00,
                         'getPriceInclTax' => 32.00,
                         'getOriginalPrice' => 32.00,
                         'getDiscountAmount' => 7.00,
@@ -96,15 +131,38 @@ class EventProcessorTest extends TestCase
                 ],
                 'expectedTotalRevenue' => 25.00
             ],
-            [ // Two items
+            [ // One item with discount (tax excluded)
+                'priceIncludesTax' => false,
                 'orderItemsData' => [
                     [
+                        'getPrice' => 25.00,
+                        'getPriceInclTax' => 32.00,
+                        'getOriginalPrice' => 25.00,
+                        'getDiscountAmount' => 7.00,
+                        'getQtyOrdered' => 1,
+                    ]
+                ],
+                'expectedResult' => [
+                    [
+                        'price' => 18.00,
+                        'discount' => 7.00,
+                        'quantity' => 1,
+                    ]
+                ],
+                'expectedTotalRevenue' => 18.00
+            ],
+            [ // Two items
+                'priceIncludesTax' => true,
+                'orderItemsData' => [
+                    [
+                        'getPrice' => 32.00,
                         'getPriceInclTax' => 32.00,
                         'getOriginalPrice' => 32.00,
                         'getDiscountAmount' => 7.00,
                         'getQtyOrdered' => 1,
                     ],
                     [
+                        'getPrice' => 32.00,
                         'getPriceInclTax' => 32.00,
                         'getOriginalPrice' => 32.00,
                         'getDiscountAmount' => 0.00,

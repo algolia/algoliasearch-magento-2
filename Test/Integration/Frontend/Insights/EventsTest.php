3e6a26758e2690c215de510fc09ea07b63f6744b
Damien Couchez <damien.couchez@gmail.com>
2025-06-26 15:15:49 +0200

    MAGE-1301: add first insights test

---
+++ Test/Integration/Frontend/Insights/EventsTest.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace Algolia\AlgoliaSearch\Test\Integration\Frontend\Insights;
+
+use Algolia\AlgoliaSearch\Api\Insights\EventProcessorInterface;
+use Algolia\AlgoliaSearch\Api\InsightsClient;
+use Algolia\AlgoliaSearch\Service\Insights\EventProcessor;
+use Algolia\AlgoliaSearch\Test\Integration\TestCase;
+use Magento\Checkout\Model\Session as CheckoutSession;
+use Magento\Framework\Api\SearchCriteriaBuilder;
+use Magento\Framework\ObjectManagerInterface;
+use Magento\Quote\Api\GuestCartManagementInterface;
+use Magento\Quote\Model\QuoteIdMaskFactory;
+use Magento\Sales\Api\Data\OrderInterface;
+use Magento\Sales\Api\OrderRepositoryInterface;
+use Magento\Sales\Model\Order;
+use Magento\Store\Model\StoreManager;
+use Magento\TestFramework\Helper\Bootstrap;
+
+/**
+ * @magentoDbIsolation enabled
+ * @magentoAppArea frontend
+ */
+class EventsTest extends TestCase
+{
+    const PLACED_ORDER_EVENT = 'Placed Order';
+    const EVENT_TYPE_CONVERSION = "conversion";
+    const EVENT_SUBTYPE_PURCHASE = "purchase";
+    const INDEX = 'my-index';
+    const TOKEN = 'dummy-token';
+
+    /**
+     * @var ObjectManagerInterface
+     */
+    protected $objectManager;
+
+    /**
+     * @var GuestCartManagementInterface
+     */
+    protected $cartManagement;
+
+    /**
+     * @var CheckoutSession
+     */
+    protected  $checkoutSession;
+
+    /**
+     * @var QuoteIdMaskFactory
+     */
+    protected $quoteIdMaskFactory;
+
+    /** @var StoreManager */
+    protected $storeManager;
+
+    /**
+     * @var EventProcessor
+     */
+    protected $eventProcessor;
+
+    /**
+     * @var InsightsClient
+     */
+    protected $insightsClient;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->objectManager = Bootstrap::getObjectManager();
+
+        $this->cartManagement = $this->objectManager->get(GuestCartManagementInterface::class);
+        $this->checkoutSession = $this->objectManager->get(CheckoutSession::class);
+        $this->quoteIdMaskFactory = $this->objectManager->get(QuoteIdMaskFactory::class);
+        $this->eventProcessor = $this->objectManager->get(EventProcessor::class);
+
+        /** @var StoreManager $storeManager */
+        $this->storeManager = $this->objectManager->get(StoreManager::class);
+
+        $this->insightsClient = $this->getMockBuilder(InsightsClient::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->eventProcessor->setInsightsClient($this->insightsClient);
+        $this->eventProcessor->setAnonymousUserToken(self::TOKEN);
+        $this->eventProcessor->setStoreManager($this->storeManager);
+    }
+
+    /**
+     * @param Order  $order
+     * @param string $eventName
+     * @return array[]
+     */
+    protected function generateConversionEvent(Order $order, string $eventName = self::PLACED_ORDER_EVENT): array
+    {
+        $event = [
+            EventProcessorInterface::EVENT_KEY_SUBTYPE => self::EVENT_SUBTYPE_PURCHASE,
+            EventProcessorInterface::EVENT_KEY_OBJECT_IDS => [],
+            EventProcessorInterface::EVENT_KEY_OBJECT_DATA => [],
+            EventProcessorInterface::EVENT_KEY_CURRENCY => 'USD',
+            EventProcessorInterface::EVENT_KEY_VALUE => 0,
+            'eventType' => self::EVENT_TYPE_CONVERSION,
+            'eventName' => $eventName,
+            'index' => self::INDEX,
+            'userToken' => self::TOKEN,
+            EventProcessorInterface::EVENT_KEY_QUERY_ID => '',
+        ];
+
+        foreach ($order->getItems() as $item) {
+            $event[EventProcessorInterface::EVENT_KEY_OBJECT_IDS][] = $item->getProductId();
+            $event[EventProcessorInterface::EVENT_KEY_OBJECT_DATA][] = [
+                'price' => $item->getPrice(),
+                'discount' => $item->getDiscountAmount(),
+                'quantity' => $item->getQtyOrdered()
+            ];
+            $event[EventProcessorInterface::EVENT_KEY_VALUE] += $item->getPrice() * $item->getQtyOrdered();
+        }
+
+        return ['events' => [$event]];
+    }
+
+    /**
+     * @param Order $order
+     * @return void
+     */
+    protected function assertOrderPurchaseEvent(Order $order): void
+    {
+        $args = [$this->generateConversionEvent($order), []];
+
+        $this->insightsClient
+            ->expects(self::once())
+            ->method('pushEvents')
+            ->with(...$args)
+            ->willReturn([]);
+
+        $this->eventProcessor->convertPurchase(
+            self::PLACED_ORDER_EVENT,
+            self::INDEX,
+            $order
+        );
+    }
+
+    /**
+     * Gets order entity by increment id.
+     *
+     * @param string $incrementId
+     * @return OrderInterface
+     */
+    protected function getOrder(string $incrementId): OrderInterface
+    {
+        /** @var SearchCriteriaBuilder $searchCriteriaBuilder */
+        $searchCriteriaBuilder = $this->objectManager->get(SearchCriteriaBuilder::class);
+        $searchCriteria = $searchCriteriaBuilder->addFilter('increment_id', $incrementId)
+            ->create();
+
+        /** @var OrderRepositoryInterface $repository */
+        $repository = $this->objectManager->get(OrderRepositoryInterface::class);
+        $items = $repository->getList($searchCriteria)
+            ->getItems();
+
+        return array_pop($items);
+    }
+
+    /**
+     * @magentoDataFixture Algolia_AlgoliaSearch::Test/Integration/Frontend/Insights/_files/order_with_two_order_items.php
+     */
+    public function testOrderWithTwoOrderItems()
+    {
+        /** @var Order $order */
+        $order = $this->getOrder('100000001');
+
+        $this->assertOrderPurchaseEvent($order);
+    }
+}

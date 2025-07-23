3be04c34bbb975402567946ce43851061cf75303
Omar Rahman <omar.rahman@algolia.com>
2025-07-23 14:50:00 -0400

    update .env and add redis

--- .circleci/install-config-mysql.php
+++ .circleci/install-config-mysql.php
@@ -23,4 +23,17 @@ return [
     'amqp-user' => 'magento',
     'amqp-password' => 'magento',
     'consumers-wait-for-messages' => '0',
+    'session-save' => 'redis',
+    'session-save-redis-host' => 'redis',
+    'session-save-redis-port' => 6379,
+    'session-save-redis-db' => 5,
+    'session-save-redis-max-concurrency' => 20,
+    'cache-backend' => 'redis',
+    'cache-backend-redis-server' => 'redis',
+    'cache-backend-redis-db' => 4,
+    'cache-backend-redis-port' => 6379,
+    'page-cache' => 'redis',
+    'page-cache-redis-server' => 'redis',
+    'page-cache-redis-db' => 3,
+    'page-cache-redis-port' => 6379
 ];

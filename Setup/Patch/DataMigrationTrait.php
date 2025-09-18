4e5a651efcb3924d162ba05c5fbc79681fd2b777
Damien Couchez <damien.couchez@gmail.com>
2025-09-18 15:48:48 +0200

    MAGE-1404: codacy warning

--- Setup/Patch/DataMigrationTrait.php
+++ Setup/Patch/DataMigrationTrait.php
@@ -14,11 +14,10 @@ trait DataMigrationTrait
         foreach ($configurations as $from => $to) {
             $configDataTable = $this->moduleDataSetup->getTable('core_config_data');
             $whereConfigPathFrom = $connection->quoteInto('path = ?', $from);
-            $whereConfigPathTo = $connection->quoteInto('path = ?', $to);
 
             $select = $connection->select()
                 ->from($configDataTable)
-                ->where($whereConfigPathTo);
+                ->where('path = ?', $to);
             $existingValues = $connection->fetchAll($select);
 
             if (count($existingValues) === 0) {

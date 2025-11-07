fa35c473c5ad3b836d4bbd10bcb45270ca42b65f
Damien Couchez <damien.couchez@gmail.com>
2025-11-07 14:33:24 +0100

    MAGE-1328: add condition for the suggestion indexing to depend on the suggestions mode

--- Service/Suggestion/BatchQueueProcessor.php
+++ Service/Suggestion/BatchQueueProcessor.php
@@ -3,8 +3,10 @@
 namespace Algolia\AlgoliaSearch\Service\Suggestion;
 
 use Algolia\AlgoliaSearch\Api\Processor\BatchQueueProcessorInterface;
+use Algolia\AlgoliaSearch\Helper\Configuration\AutocompleteHelper;
 use Algolia\AlgoliaSearch\Helper\Data;
 use Algolia\AlgoliaSearch\Model\Queue;
+use Algolia\AlgoliaSearch\Model\Source\Suggestions;
 use Algolia\AlgoliaSearch\Service\AlgoliaCredentialsManager;
 use Algolia\AlgoliaSearch\Service\Suggestion\IndexBuilder as SuggestionIndexBuilder;
 
@@ -12,13 +14,15 @@ class BatchQueueProcessor implements BatchQueueProcessorInterface
 {
     public function __construct(
         protected Data $dataHelper,
+        protected AutocompleteHelper $autocompleteHelper,
         protected Queue $queue,
         protected AlgoliaCredentialsManager $algoliaCredentialsManager
     ){}
 
     public function processBatch(int $storeId, ?array $entityIds = null): void
     {
-        if ($this->dataHelper->isIndexingEnabled($storeId) === false) {
+        if ($this->dataHelper->isIndexingEnabled($storeId) === false
+            || $this->autocompleteHelper->getSuggestionsMode($storeId) !== Suggestions::SUGGESTIONS_MAGENTO) {
             return;
         }
 

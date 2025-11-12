6b36823caf296c7e3115242f3f6ea490fb241bba
Damien Couchez <damien.couchez@gmail.com>
2025-11-12 10:07:06 +0100

    MAGE-1328: remove indexing control

--- Service/Suggestion/BatchQueueProcessor.php
+++ Service/Suggestion/BatchQueueProcessor.php
@@ -3,10 +3,8 @@
 namespace Algolia\AlgoliaSearch\Service\Suggestion;
 
 use Algolia\AlgoliaSearch\Api\Processor\BatchQueueProcessorInterface;
-use Algolia\AlgoliaSearch\Helper\Configuration\AutocompleteHelper;
 use Algolia\AlgoliaSearch\Helper\Data;
 use Algolia\AlgoliaSearch\Model\Queue;
-use Algolia\AlgoliaSearch\Model\Source\Suggestions;
 use Algolia\AlgoliaSearch\Service\AlgoliaCredentialsManager;
 use Algolia\AlgoliaSearch\Service\Suggestion\IndexBuilder as SuggestionIndexBuilder;
 
@@ -14,15 +12,13 @@ class BatchQueueProcessor implements BatchQueueProcessorInterface
 {
     public function __construct(
         protected Data $dataHelper,
-        protected AutocompleteHelper $autocompleteHelper,
         protected Queue $queue,
         protected AlgoliaCredentialsManager $algoliaCredentialsManager
     ){}
 
     public function processBatch(int $storeId, ?array $entityIds = null): void
     {
-        if ($this->dataHelper->isIndexingEnabled($storeId) === false
-            || $this->autocompleteHelper->getSuggestionsMode($storeId) !== Suggestions::SUGGESTIONS_MAGENTO) {
+        if ($this->dataHelper->isIndexingEnabled($storeId) === false) {
             return;
         }
 

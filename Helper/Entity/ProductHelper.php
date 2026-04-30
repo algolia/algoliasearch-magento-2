74d8161c6bde3cd1123c109ee32dad717b92a399
Damien Couchez <damien.couchez@gmail.com>
2026-04-30 15:25:39 +0200

    MAGE-1569: remove waitLastTask for query rules creation

--- Helper/Entity/ProductHelper.php
+++ Helper/Entity/ProductHelper.php
@@ -354,11 +354,9 @@ class ProductHelper extends AbstractEntityHelper
         }
 
         $this->setFacetsQueryRules($indexOptions);
-        $this->algoliaConnector->waitLastTask($storeId);
 
         if ($saveToTmpIndicesToo) {
             $this->setFacetsQueryRules($indexTmpOptions);
-            $this->algoliaConnector->waitLastTask($storeId);
         }
 
         $this->replicaManager->syncReplicasToAlgolia($storeId, $indexSettings);
@@ -479,11 +477,12 @@ class ProductHelper extends AbstractEntityHelper
 
     /**
      * @param IndexOptionsInterface $indexOptions
+     * @param bool $waitLastTask
      * @return void
      * @throws AlgoliaException
      * @throws NoSuchEntityException
      */
-    protected function setFacetsQueryRules(IndexOptionsInterface $indexOptions)
+    public function setFacetsQueryRules(IndexOptionsInterface $indexOptions, bool $waitLastTask = false): void
     {
         $this->clearFacetsQueryRules($indexOptions);
 
@@ -521,6 +520,10 @@ class ProductHelper extends AbstractEntityHelper
             $this->logger->log('Setting facets query rules to "' . $indexOptions->getIndexName() . '" index: ' . json_encode($rules));
 
             $this->algoliaConnector->saveRules($indexOptions, $rules, true);
+
+            if ($waitLastTask) {
+                $this->algoliaConnector->waitLastTask($indexOptions->getStoreId());
+            }
         }
     }
 

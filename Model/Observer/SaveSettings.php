2ef4d41f175c4d9d1dc749bf658c0159956d9e9b
Damien Couchez <damien.couchez@gmail.com>
2026-05-18 15:50:31 +0200

    MAGE-1570: apply settings diff on batch process

--- Model/Observer/SaveSettings.php
+++ Model/Observer/SaveSettings.php
@@ -8,10 +8,19 @@ use Algolia\AlgoliaSearch\Helper\Entity\ProductHelper;
 use Algolia\AlgoliaSearch\Model\IndicesConfigurator;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Store\Model\StoreManagerInterface;
 
 class SaveSettings implements ObserverInterface
 {
+    public const EVENTS = [
+        'admin_system_config_changed_section_algoliasearch_instant'    => ['products'],
+        'admin_system_config_changed_section_algoliasearch_images'     => ['products'],
+        'admin_system_config_changed_section_algoliasearch_products'   => ['products'],
+        'admin_system_config_changed_section_algoliasearch_categories' => ['categories']
+    ];
+
     public function __construct(
         protected StoreManagerInterface $storeManager,
         protected IndicesConfigurator $indicesConfigurator,
@@ -23,13 +32,17 @@ class SaveSettings implements ObserverInterface
      * @param Observer $observer
      *
      * @throws AlgoliaException
+     * @throws LocalizedException
+     * @throws NoSuchEntityException
      */
     public function execute(Observer $observer)
     {
+        $filteredEntities = self::EVENTS[$observer->getEvent()->getName()] ?? [];
+
          try {
             $storeIds = array_keys($this->storeManager->getStores());
             foreach ($storeIds as $storeId) {
-                $this->indicesConfigurator->saveConfigurationToAlgolia($storeId);
+                $this->indicesConfigurator->saveConfigurationToAlgolia($storeId, false, $filteredEntities);
             }
         } catch (\Exception $e) {
             if ($e->getCode() !== 404) {

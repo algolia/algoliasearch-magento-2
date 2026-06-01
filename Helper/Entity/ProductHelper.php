6214ebef44aa0d28d8cf47357f1b2bb4f63a26f1
Damien Couchez <damien.couchez@gmail.com>
2026-06-01 16:51:09 +0200

    MAGE-1573: remove param from setQueryRules method

--- Helper/Entity/ProductHelper.php
+++ Helper/Entity/ProductHelper.php
@@ -455,12 +455,11 @@ class ProductHelper extends AbstractEntityHelper
 
     /**
      * @param IndexOptionsInterface $indexOptions
-     * @param bool $collectTaskId
      * @return void
      * @throws AlgoliaException
      * @throws NoSuchEntityException
      */
-    protected function setFacetsQueryRules(IndexOptionsInterface $indexOptions, bool $collectTaskId = false): void
+    protected function setFacetsQueryRules(IndexOptionsInterface $indexOptions): void
     {
         $this->clearFacetsQueryRules($indexOptions);
 
@@ -498,10 +497,6 @@ class ProductHelper extends AbstractEntityHelper
             $this->logger->log('Setting facets query rules to "' . $indexOptions->getIndexName() . '" index: ' . json_encode($rules));
 
             $this->algoliaConnector->saveRules($indexOptions, $rules, true);
-
-            if ($collectTaskId) {
-                $this->algoliaConnector->collectTaskIdToWaitFor($indexOptions);
-            }
         }
     }
 

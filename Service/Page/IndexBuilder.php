27411b96d109ddf73855eab038005d20e394f80d
Damien Couchez <damien.couchez@gmail.com>
2026-06-25 10:54:44 +0200

    MAGE-1625: reorg page indexing process as per review

--- Service/Page/IndexBuilder.php
+++ Service/Page/IndexBuilder.php
@@ -69,6 +69,7 @@ class IndexBuilder extends AbstractIndexBuilder implements IndexBuilderInterface
 
         $indexOptions = $this->indexOptionsBuilder->buildEntityIndexOptions($storeId);
 
+
         $this->startEmulation($storeId);
 
         $pages = $this->pageHelper->getPages($storeId, $entityIds);
@@ -78,9 +79,10 @@ class IndexBuilder extends AbstractIndexBuilder implements IndexBuilderInterface
         // if there are pageIds defined, do not index to _tmp
         $isFullReindex = (!$entityIds);
 
+        $toIndexOptions = $this->indexOptionsBuilder->buildEntityIndexOptions($storeId, $isFullReindex);
+
         if (isset($pages['toIndex']) && count($pages['toIndex'])) {
             $pagesToIndex = $pages['toIndex'];
-            $toIndexOptions = $this->indexOptionsBuilder->buildEntityIndexOptions($storeId, $isFullReindex);
 
             foreach (array_chunk($pagesToIndex, 100) as $chunk) {
                 try {
@@ -89,9 +91,6 @@ class IndexBuilder extends AbstractIndexBuilder implements IndexBuilderInterface
                     $this->logger->log($e->getMessage());
                     continue;
                 }
-            }
-            if ($isFullReindex) {
-                $this->moveTemporaryIndex($indexOptions, $toIndexOptions);
             }
         }
 
@@ -105,6 +104,10 @@ class IndexBuilder extends AbstractIndexBuilder implements IndexBuilderInterface
                     continue;
                 }
             }
+        }
+
+        if ($isFullReindex) {
+            $this->moveTemporaryIndex($indexOptions, $toIndexOptions);
         }
     }
 }

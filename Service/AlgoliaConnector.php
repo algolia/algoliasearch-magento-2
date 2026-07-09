ee1e086ca6e5d38f27ed447823877d97bdbd5b21
Eric Wright <eric.wright@algolia.com>
2026-07-09 17:41:55 -0400

    MAGE-1648 Strip semanticSearch from mergeSettings

--- Service/AlgoliaConnector.php
+++ Service/AlgoliaConnector.php
@@ -438,7 +438,7 @@ class AlgoliaConnector
      */
     protected function getSettingsToRemove(array $onlineSettings): array
     {
-        $removals = ['slaves', 'replicas', 'decompoundedAttributes'];
+        $removals = ['slaves', 'replicas', 'decompoundedAttributes', 'semanticSearch'];
 
         if (isset($onlineSettings['mode']) && $onlineSettings['mode'] == 'neuralSearch') {
             $removals[] = 'mode';

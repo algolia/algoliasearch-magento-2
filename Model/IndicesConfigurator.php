21379fd93e9f4547b2f087f8d96f375a359e7a35
Damien Couchez <damien.couchez@gmail.com>
2026-09-14 11:01:46 +0200

    MAGE-1704: introduced asynchronous config save feature

--- Model/IndicesConfigurator.php
+++ Model/IndicesConfigurator.php
@@ -55,7 +55,8 @@ class IndicesConfigurator
     public function saveConfigurationToAlgolia(
         int $storeId,
         bool $useTmpIndex = false,
-        array $filteredEntities = [])
+        array $filteredEntities = [],
+        bool $skipWait = false)
     : void
     {
         $logEventName = 'Save configuration to Algolia for store: ' . $this->logger->getStoreName($storeId);
@@ -97,6 +98,11 @@ class IndicesConfigurator
         }
 
         $this->setExtraSettings($storeId, $useTmpIndex, $filteredEntities);
+
+        if ($this->configHelper->isAsyncConfigSaveEnabled($storeId) && $skipWait) {
+            $this->logger->stop($logEventName, true, true);
+            return;
+        }
 
         $this->algoliaConnector->waitForAllCollectedTaskIds($storeId);
 
